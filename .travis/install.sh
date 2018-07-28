dde42fc5b30d882ab9a07d2676c094562f2dedb1
chrBrd <chris@binaryspanner.com>
2018-07-28 04:50:32 +0100

    Finally worked.

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,33 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#set -e
-
-foo() {
+error_exit() {
     echo "quitting"
-    #cat config.log
     exit 1
 }
 
-
-trap "foo" ERR
-set -x
-
-if [[ -n "$PYENV_VERSION" ]]; then
-    if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-        brew update
-        brew upgrade readline openssl
-        brew cask uninstall oclint
-        brew install expat xz gcc
-        brew upgrade pyenv
+install_pyenv() {
+    set -x
+    if [[ -n "$PYENV_VERSION" ]]; then
+        if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+            brew update  > /dev/null
+            brew upgrade readline openssl
+            #brew cask uninstall oclint
+            #brew install expat xz gcc
+            brew upgrade pyenv
+        fi
+        eval "$(pyenv init -)"
+        #pyenv install --list
+        if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+            #CFLAGS="-I$(xcrun --show-sdk-path)/usr/include /usr/local/opt/expat/include $CFLAGS" \
+            pyenv install -s "$PYENV_VERSION"
+        else
+            pyenv install -s "$PYENV_VERSION"
+        fi
+        pyenv rehash
+        python -m pip install wheel
     fi
-    eval "$(pyenv init -)"
-    #pyenv install --list
-    if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-        #CFLAGS="-I$(xcrun --show-sdk-path)/usr/include /usr/local/opt/expat/include $CFLAGS" \
-        pyenv install -s "$PYENV_VERSION"
-    else
-        pyenv install -s "$PYENV_VERSION"
-    fi
-    pyenv rehash
-    python -m pip install wheel
-fi
+}
+
+trap "error_exit" ERR
+install_pyenv
+
